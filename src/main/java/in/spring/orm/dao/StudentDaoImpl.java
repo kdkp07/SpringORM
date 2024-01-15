79de1e5ab161e5900cca2de0aa29eb85aaecf63f
Kuldeep Patidar <patidarkuldeep660@gmail.com>
2024-01-15 10:11:02 +0530

    Implemented insert method and added Transactional annotation

--- src/main/java/in/spring/orm/dao/StudentDaoImpl.java
+++ src/main/java/in/spring/orm/dao/StudentDaoImpl.java
@@ -2,16 +2,28 @@ package in.spring.orm.dao;
 
 import in.spring.orm.entities.Student;
 import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-public class StudentDaoImpl implements StudentDao{
+@Repository
+public class StudentDaoImpl{
 
     private HibernateTemplate hibernateTemplate;
 
 
     // insert student
-    @Override
+
+    @Transactional(readOnly = false)              // in order to enable write operation
     public int insert(Student student) {
+
         int r= (int) this.hibernateTemplate.save(student);
         return r;
+    }
+    public HibernateTemplate getHibernateTemplate() {
+        return hibernateTemplate;
+    }
+
+    public void setHibernateTemplate(HibernateTemplate hibernateTemplate) {
+        this.hibernateTemplate = hibernateTemplate;
     }
 }
