693fd22e7bd0abf09b562afc31cd0ea176aa71ba
Kuldeep Patidar <patidarkuldeep660@gmail.com>
2024-01-15 10:08:55 +0530

    Added Main method

--- src/main/java/in/spring/orm/App.java
+++ src/main/java/in/spring/orm/App.java
@@ -1,9 +1,23 @@
 package in.spring.orm;
 
-public class App 
+import in.spring.orm.dao.StudentDao;
+import in.spring.orm.dao.StudentDaoImpl;
+import in.spring.orm.entities.Student;
+import in.spring.orm.resources.springConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ApplicationContext context = new AnnotationConfigApplicationContext(springConfig.class);
+//        ApplicationContext context = new ClassPathXmlApplicationContext("in/spring/orm/resources/config.xml");
+        StudentDaoImpl studentDao = context.getBean("getStdDao", StudentDaoImpl.class);
+
+        Student student = new Student(112,"Aakash Kumar","Delhi");
+        int r = studentDao.insert(student);
+        System.out.println("Number of rows affected " + r);
     }
 }
